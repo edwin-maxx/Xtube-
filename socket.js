e0c69ee1761dbb27dd6d87bcbe416e5d5ba0c80c
Redwan Ahemed <rashidakhatun2009@gmail.com>
2026-01-08 23:24:35 +0600

    Add online user tracking to socket connections
    Track the number of online users and emit updates on connection and disconnection.

--- socket.js
+++ socket.js
@@ -3,10 +3,20 @@ const Message = require("./models/Message");
 module.exports = (server) => {
   const io = require("socket.io")(server);
 
+  let online = 0;
+
   io.on("connection", (socket) => {
+    online++;
+    io.emit("online-users", online);
+
     socket.on("chat-message", async (msg) => {
       const saved = await Message.create(msg);
       io.emit("chat-message", saved);
+    });
+
+    socket.on("disconnect", () => {
+      online--;
+      io.emit("online-users", online);
     });
   });
 };
