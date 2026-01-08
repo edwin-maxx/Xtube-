c62271c4a5675d1cdddf403447ad2f13c0cc855b
Redwan Ahemed <rashidakhatun2009@gmail.com>
2026-01-08 21:53:04 +0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const http = require("http");
+
+const app = express();
+const server = http.createServer(app);
+
+
+require("./socket")(server);
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(console.error);
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(express.static("tube"));
+
+app.use("/api/videos", require("./routes/videos"));
+app.use("/api/upload", require("./routes/upload"));
+app.use("/api/comments", require("./routes/comments"));
+
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/tube/home.html");
+});
+
+app.get("/watch", (req, res) => {
+  res.sendFile(__dirname + "/tube/watch.html");
+});
+
+app.get("/upload", (req, res) => {
+  res.sendFile(__dirname + "/tube/upload.html");
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () =>
+  console.log(`Server running → http://localhost:${PORT}`)
+);
+              
