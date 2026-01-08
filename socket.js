40778dc846f6a9465d7b97124fd10055faede6bf
Xe Mo <callmexemon@gmail.com>
2026-01-09 02:00:09 +0600

    Update socket.js

--- socket.js
+++ socket.js
@@ -3,20 +3,48 @@ const Message = require("./models/Message");
 module.exports = (server) => {
   const io = require("socket.io")(server);
 
-  let online = 0;
+  const onlineUsers = new Map(); // socket.id -> username
 
   io.on("connection", (socket) => {
-    online++;
-    io.emit("online-users", online);
 
-    socket.on("chat-message", async (msg) => {
-      const saved = await Message.create(msg);
-      io.emit("chat-message", saved);
+    // ----- Join Room -----
+    socket.on("join-room", ({ roomId, username }) => {
+      socket.join(roomId);
+      onlineUsers.set(socket.id, username);
+
+      // Emit online users count
+      io.emit("online-users", Array.from(onlineUsers.values()));
+
+      // Notify others
+      socket.to(roomId).emit("user-joined", { id: socket.id, username });
+
+      // ----- WebRTC signaling -----
+      socket.on("signal", ({ target, signal }) => {
+        io.to(target).emit("signal", { from: socket.id, signal });
+      });
+
+      // ----- Video sync -----
+      socket.on("video-sync", data => {
+        socket.to(data.roomId).emit("video-sync", data);
+      });
+
+      // ----- Global chat -----
+      socket.on("chat-message", async (msg) => {
+        const saved = await Message.create(msg);
+        io.emit("chat-message", saved);
+      });
     });
 
+    // ----- Disconnect -----
     socket.on("disconnect", () => {
-      online--;
-      io.emit("online-users", online);
+      const username = onlineUsers.get(socket.id);
+      onlineUsers.delete(socket.id);
+
+      // Emit updated online users
+      io.emit("online-users", Array.from(onlineUsers.values()));
+
+      // Notify rooms about leaving
+      socket.broadcast.emit("user-left", socket.id);
     });
   });
 };
